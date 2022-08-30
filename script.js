87c25422f7aab9eb12fda45dbf1fdf0dd9d07932
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-08-30 15:06:00 +1000

    Updated javascript

--- script.js
+++ script.js
@@ -1,76 +1,85 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+document.addEventListener("DOMContentLoaded", function () {
+  const generateBtn = document.querySelector("#generate");
+  generateBtn.addEventListener("click", execute);
+});
 
-// Write password to the #password input
+const lowerC = "abcdefghijklmnopqrstuvwxyz";
+const upperC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialC = "!@#$%^&*()";
 
-// var lCase = "abcdefghijklmnopqrstuvwxyz";
-// var uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// var numbers = "0123456789";
-// var sChar = ".[]{}()<>*+-=!?^$|/%@#~";
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-
-function generatePassword(pass_length) {
-  let password = '';
-  let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let passwordLength = pass_length;
- 
-  let array = [];
-  for (let i=0; i<pass_length; i++){
-    array.push(Math.floor(Math.random() * chars.length)); 
-  }
- 
-  for (let i = 0; i < passwordLength; i++) {
-    password += chars[array[i]];
-  }
- 
-  const msg = `Your new password is: "${password}"`
-  console.log(msg);
-  document.getElementById('password').value = 'Your password is: ' + password;
-  return password;
+function execute() {
+  const user = userPref();
+  const passGenerated = generatePassword(
+    user.passLength,
+    user.lowerCase,
+    user.upperCase,
+    user.num,
+    user.specialChar
+  );
+  writePassword(passGenerated);
 }
 
 function userPref() {
-  console.log("Function triggered");
-  var pass_length = window.prompt("Enter your length of Password");
-  
-  var lowercase = confirm("Include lowercase?");
-  var uppercase = confirm("Include UPPERCASE?");
-  var num = confirm("Include Numbers?");
-  var s_char = confirm("Include Special Characters?");
-
-  generatePassword(pass_length);
-  if (pass_length < 8) {
-    alert("Password too short");
-    exit();
-  } else if (pass_length > 128) {
-    alert("Password too long");
-    exit();
+  const passLength = window.prompt("Enter your length of Password");
+  const validation = validate(passLength);
+  if (validation !== "") {
+    alert(validation);
+    return;
   }
-  
-  return pass_length;
-  
 
+  const lowerCase = confirm("Include lower case characters?");
+  const upperCase = confirm("Include upper case characters?");
+  const num = confirm("Include numbers?");
+  const specialChar = confirm("Include special characters?");
+
+  return {
+    passLength,
+    lowerCase,
+    upperCase,
+    num,
+    specialChar,
+  };
 }
 
-// generateBtn.addEventListener("click", writePassword);
-var el = document.getElementById("generate")
-el.addEventListener("click", userPref);
+function validate(passLength) {
+  if (passLength < 8) {
+    return "Password too short";
+  } else if (passLength > 128) {
+    return "Password too long";
+  }
+  return "";
+}
 
+function generatePassword(passLength, lowerCase, upperCase, num, specialChar) {
+  let availableC = "";
+  let password = "";
 
-// for (var i = 0; i < pass_length; i++) {
-//   if (lowercase) {
-//     var rNum = Math.floor(Math.ramdom() * pass_length);
-//     randomString += lCase.substring(rNum, rNum + 1);
-//     console.log(randomString);
-//   } else if (uppercase) {
-//   } else if (num) {
-//   } else if (s_char) {
-//   }
+  if (lowerCase) {
+    availableC += lowerC;
+  }
+  if (upperCase) {
+    availableC += upperC;
+  }
+  if (num) {
+    availableC += numbers;
+  }
+  if (specialChar) {
+    availableC += specialC;
+  }
+
+  for (var i = 0; i < passLength; i++) {
+    const randomIndex = Math.floor(Math.random() * availableC.length);
+    password += availableC.charAt(randomIndex);
+  }
+  console.log(passLength);
+  console.log(password);
+
+  return password;
+}
+
+function writePassword(password) {
+  const passwordText = document.querySelector("#password");
+  console.log(password);
+  passwordText.value = password;
+}
